917e4980105800bce2c43d19b6d68879a2a8d1ef
Ryan "archaen" Pando <vretha@gmail.com>
2016-09-19 16:15:33 -0600

    Express Generator

--- src/server/routes/reviews.js
+++ src/server/routes/reviews.js
@@ -44,4 +44,44 @@ router.post('/:id/reviews/new', indexController.isAuthenticated, (req, res, next
   });
 });
 
+router.delete('/:id/reviews/new', (req, res, next) => {
+    const id = parseInt(req.params.id);
+    return knex('reviews').del().where('restaurant_id', id)
+  .then(() => {
+    window.alert("Successfully Deleted");
+    res.render();
+  })
+  .catch((err) => {
+    return next(err);
+  });
+});
+
+router.put('/:id/reviews/edit', indexController.isAuthenticated, (req, res, next) => {
+    // grab the values to add to the db via req.body
+    const id = parseInt(req.params.id); //id is restaurant_id
+    const rating = req.body.rating;
+    const reviewer_name = req.body.reviewer_name;
+    const date = req.body.date;
+    const text = req.body.text;
+    const user_id = req.body.user_id;
+    // add values to database
+    knex('reviews').update({
+      restaurant_id: id,
+      rating: rating,
+      reviewer_name: reviewer_name,
+      date: date,
+      text: text,
+      user_id: user_id
+    })
+    .where(review_id, )
+    .then((results) => {
+      // redirect user
+      window.alert("Successfully Edited");
+      res.redirect(`/restaurants/${id}`);
+    })
+    .catch((err) => {
+      return next(err);
+    });
+  });
+
 module.exports = router;
